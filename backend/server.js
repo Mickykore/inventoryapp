2220fb59c3f4e08e4b21cd0c4f55541283273441
Mickykore <mikiyasayele6@gmail.com>
2024-07-11 12:36:32 +0300

    Update CORS origin in server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ['http://localhost:3000', "https://yebirhan-inventoryapp.vercel.app/"], 
+        origin: ['http://localhost:3000', "https://yebirhan-inventoryapp.vercel.app"], 
         credentials: true
     }
 ));
